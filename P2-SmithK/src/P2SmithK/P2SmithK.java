275d77f8ce31a231ad59998bc77faa9e89e5a825
Kaden Smith <kaden.smith0124@gmail.com>
2025-04-02 17:54:10 -0500

    generated weights and values

--- P2-SmithK/src/P2SmithK/P2SmithK.java
+++ P2-SmithK/src/P2SmithK/P2SmithK.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package P2SmithK;
 
-/**
- *
- * @author s550883
- */
+import java.util.Random;
+
 public class P2SmithK {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int itemCount = 1000;
+        int maxWeight = 100;
+        int maxValue = 500;
+        int[] weights = new int[itemCount];
+        int[] values = new int[itemCount];
+        
+        assignWeight(itemCount, maxWeight, weights);
+        assignValue(itemCount, maxValue, values);
+
+        
+
+        // Prints lists (comment out when not needed)
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Item " + i + " -> Weight: " + weights[i] + ", Value: " + values[i]);
+        }
+        
+    }
+    
+    public static void assignWeight(int count, int weight, int [] arr)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < count; i++) {
+            arr[i] = rand.nextInt(weight) + 1;
+        }
+    }
+    
+    public static void assignValue(int count, int value, int [] arr)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < count; i++) {
+            arr[i] = rand.nextInt(value) + 1;
+        }
     }
     
 }
